b0ab6b9c71f802fc0c44cef5446957feb44b1bfd
Fernando Claverino <fernando.claverino@gmail.com>
2024-12-17 19:25:27 -0300

    fix seed

--- lib/seed.ts
+++ lib/seed.ts
@@ -1,14 +1,14 @@
-import { sql } from '@vercel/postgres';
-import fs from 'fs';
-import csv from 'csv-parser';
-import path from 'path';
-import "dotenv/config"
+import { sql } from "@vercel/postgres";
+import fs from "fs";
+import csv from "csv-parser";
+import path from "path";
+import "dotenv/config";
 
 function parseDate(dateString: string): string {
-  const parts = dateString.split('/');
+  const parts = dateString.split("/");
   if (parts.length === 3) {
-    const day = parts[0].padStart(2, '0');
-    const month = parts[1].padStart(2, '0');
+    const day = parts[1].padStart(2, "0");
+    const month = parts[0].padStart(2, "0");
     const year = parts[2];
     return `${year}-${month}-${day}`;
   }
@@ -33,29 +33,29 @@ export async function seed() {
   console.log(`Created "unicorns" table`);
 
   const results: any[] = [];
-  const csvFilePath = path.join(process.cwd(), 'unicorns.csv');
+  const csvFilePath = path.join(process.cwd(), "unicorns.csv");
 
   await new Promise((resolve, reject) => {
     fs.createReadStream(csvFilePath)
       .pipe(csv())
-      .on('data', (data) => results.push(data))
-      .on('end', resolve)
-      .on('error', reject);
+      .on("data", (data) => results.push(data))
+      .on("end", resolve)
+      .on("error", reject);
   });
 
   for (const row of results) {
-    const formattedDate = parseDate(row['Date Joined']);
+    const formattedDate = parseDate(row["Date Joined"]);
 
     await sql`
       INSERT INTO unicorns (company, valuation, date_joined, country, city, industry, select_investors)
       VALUES (
         ${row.Company},
-        ${parseFloat(row['Valuation ($B)'].replace('$', '').replace(',', ''))},
+        ${parseFloat(row["Valuation ($B)"].replace("$", "").replace(",", ""))},
         ${formattedDate},
         ${row.Country},
         ${row.City},
         ${row.Industry},
-        ${row['Select Investors']}
+        ${row["Select Investors"]}
       )
       ON CONFLICT (company) DO NOTHING;
     `;
@@ -68,6 +68,5 @@ export async function seed() {
     unicorns: results,
   };
 }
-
 
 seed().catch(console.error);
